6f8c72f355c29af1a319c8331b1f68ab69aed43e
pengyuliang <u202242132@xs.ustb.edu.cn>
2024-10-17 23:27:52 +0800

    1

--- Circuit.h
+++ Circuit.h
@@ -20,8 +20,7 @@ struct GNode
 
 class Circuit
 {
-	vector<LogicGate> Gates;//逻辑门集合，即图中节点集合
-							//逻辑门以在数组中的编号作为唯一标识
+	unordered_map<string, LogicGate> Gates;//逻辑门集合，即图中节点集合		
 
 	vector<vector<int>> adj;//邻接表
 
@@ -33,49 +32,49 @@ public:
 
 	//读入逻辑门时，构建输出信号和输入信号的映射
 	//
-	Circuit(const vector<LogicGate>& Nodes, const unordered_map<string, int>& outputToGate, const unordered_map<string, vector<int>>& inputToGate)
+	Circuit(const unordered_map<string, LogicGate>& Nodes, const unordered_map<string, string>& outputToGate, const unordered_map<string, vector<string>>& inputToGate)
 	{
 		Gates = Nodes;
 
 
 	}
 
-	Circuit(const vector<LogicGate>& Nodes)
-	{
-		Gates = Nodes;
-		int NodesSize = static_cast<int>(Nodes.size());
+	//Circuit(const vector<LogicGate>& Nodes)
+	//{
+	//	Gates = Nodes;
+	//	int NodesSize = static_cast<int>(Nodes.size());
 
-		unordered_map<string, vector<int>> input;//键值为输入信号名称，值为逻辑门编号
-		unordered_map<string , int> output;//键值为输出信号名称，值为逻辑门编号
+	//	unordered_map<string, vector<int>> input;//键值为输入信号名称，值为逻辑门编号
+	//	unordered_map<string , int> output;//键值为输出信号名称，值为逻辑门编号
 
-		//记录逻辑门的输入和输出情况
-		for (int i = 0; i < NodesSize; i++)
-		{
-			output.insert({ Gates[i].port[0], i });
+	//	//记录逻辑门的输入和输出情况
+	//	for (int i = 0; i < NodesSize; i++)
+	//	{
+	//		output.insert({ Gates[i].port[0], i });
 
-			input[Gates[i].port[1]].push_back(i);
-			if (Gates[i].type != 3)
-			{
-				input[Gates[i].port[2]].push_back(i);
-			}
-		}
+	//		input[Gates[i].port[1]].push_back(i);
+	//		if (Gates[i].type != 3)
+	//		{
+	//			input[Gates[i].port[2]].push_back(i);
+	//		}
+	//	}
 
 
-		//根据逻辑门的输入输出表，添加图的边
-		unordered_map<string, int >::iterator iter_out = output.begin();
-		unordered_map<string, vector<int>>::iterator iter_in;
-		for (; iter_out != output.end(); iter_out++)
-		{
-			//如果在输入表中找到了某个输出信号，则添加输出逻辑门指向输入逻辑门的边
-			iter_in = input.find(iter_out->first);
-			if (iter_in != input.end())
-			{
-				addEdge(iter_out->second, iter_in->second);
-			}
+	//	//根据逻辑门的输入输出表，添加图的边
+	//	unordered_map<string, int >::iterator iter_out = output.begin();
+	//	unordered_map<string, vector<int>>::iterator iter_in;
+	//	for (; iter_out != output.end(); iter_out++)
+	//	{
+	//		//如果在输入表中找到了某个输出信号，则添加输出逻辑门指向输入逻辑门的边
+	//		iter_in = input.find(iter_out->first);
+	//		if (iter_in != input.end())
+	//		{
+	//			addEdge(iter_out->second, iter_in->second);
+	//		}
 
-		}
+	//	}
 
-	}
+	//}
 
 };
 
--- main.cpp
+++ main.cpp
@@ -11,6 +11,13 @@ int main() {
 	* 随后为输出、输入信号
 	*/
 
+	//无法在读入时使用下标作为逻辑门的唯一标识
+	//读入时可以用逻辑门名称作为唯一标识
+	//读入时用三个unordered_map
+	// unordered_map<string,<LogicGate>>，键为逻辑门名称、值为逻辑门对象
+	// unordered_map<string,string>，键为输出信号名称、值为逻辑门名称（每个逻辑门输出信号均不同，无需考虑哈希表覆盖问题）
+	// unordered_map<string,vector<string>>，键为输入信号名称、值为逻辑门名称数组（一个信号可能输入进多个逻辑门）
+
 
 
 }
