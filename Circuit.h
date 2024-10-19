cc7c01bfbc5fbf84c85f705a0ef006a936e495a4
pengyuliang <u202242132@xs.ustb.edu.cn>
2024-10-19 15:07:17 +0800

    DFS

--- Circuit.h
+++ Circuit.h
@@ -2,83 +2,97 @@
 #define CIRCUIT_H
 
 #include<unordered_map>
+#include<unordered_set>
 #include"Logic_Gate.h"
-using namespace std;
 
 /*
 建立起图之后，无需对其进行添加删除操作
 VPI接口无法解析出逻辑门的邻接门，需要根据逻辑门的输入输出自行建立节点的连接情况
 */
 
-//邻接表结点
-struct GNode
-{
-	LogicGate Gate;
-	GNode* next;
-};
-
-
 class Circuit
 {
-	unordered_map<string, LogicGate> Gates;//逻辑门集合，即图中节点集合		
+	std::unordered_map<std::string, LogicGate> Gates;//逻辑门集合，即图中节点集合		
 	//逻辑门名称作为识别逻辑门的唯一标识
 
-	unordered_map<string, vector<LogicGate>>adj;
+	std::unordered_map<std::string, std::vector<std::string>>adj;
+	std::unordered_set<std::string>visited;
+	std::unordered_set<std::string>recStack;
+	std::vector<std::vector<std::string>> circles; // 存储所有环
 
 
 public:
 	
-	//addEdge待补充
-	void addEdge(int start, vector<int> end){};
+	void addEdge(std::string from,const std::vector<std::string>& to)
+	{
+		for (auto& it : to)
+		{
+			adj[from].push_back(it);
+		}
+	};
 
-	
 	//根据逻辑门表，输入输出表建立有向图
-	Circuit(const unordered_map<string, LogicGate>& Nodes, const unordered_map<string, string>& outputToGate, const unordered_map<string, vector<string>>& inputToGate)
+	Circuit(const std::unordered_map<std::string, LogicGate>& Nodes, const std::unordered_map<std::string, std::string>& outputToGate, const std::unordered_map<std::string, std::vector<std::string>>& inputToGate)
 	{
 		//接收有向图节点
 		Gates = Nodes;
 
-
+		//遍历输出
+		//若在输入中找到输入信号，则为输出逻辑门和输入逻辑门组添加边
+		for (const auto& output : outputToGate)
+		{
+			auto it = inputToGate.find(output.first);
+			if (it != inputToGate.end())
+			{
+				addEdge(output.second,it->second);
+			}
+		}
 
 	}
 
-	//Circuit(const vector<LogicGate>& Nodes)
-	//{
-	//	Gates = Nodes;
-	//	int NodesSize = static_cast<int>(Nodes.size());
+	void DFSUtil(std::string v, std::unordered_set<std::string>& visited, std::unordered_set<std::string>& recStack, std::vector<std::string>& path)
+	{
+		visited.insert(v);
+		recStack.insert(v);
+		path.push_back(v);
+		
+		for (std::string next : adj[v])
+		{
+			if (visited.find(next) == visited.end())//邻接节点没有被访问
+			{
+				DFSUtil(next, visited, recStack, path);
+			}
+			else if (recStack.find(next) != recStack.end())//邻接节点在递归栈中，表明找到了一个环路
+			{
+				std::vector<std::string> circle;
+				for (size_t i = path.size() - 1; i != 0; --i) //逆向遍历，将路径记录到Circle中
+				{
+					if (path[i] == next) 
+					{
+						circle.insert(circle.begin(), path[i]);
+						break;
+					}
+					circle.insert(circle.begin(), path[i]);
+				}
+				circle.push_back(next);
+				circles.push_back(circle);
+			}
+		}
 
-	//	unordered_map<string, vector<int>> input;//键值为输入信号名称，值为逻辑门编号
-	//	unordered_map<string , int> output;//键值为输出信号名称，值为逻辑门编号
+	};
 
-	//	//记录逻辑门的输入和输出情况
-	//	for (int i = 0; i < NodesSize; i++)
-	//	{
-	//		output.insert({ Gates[i].port[0], i });
+	void detectCircle()
+	{
+		std::vector<std::string>path;
 
-	//		input[Gates[i].port[1]].push_back(i);
-	//		if (Gates[i].type != 3)
-	//		{
-	//			input[Gates[i].port[2]].push_back(i);
-	//		}
-	//	}
-
-
-	//	//根据逻辑门的输入输出表，添加图的边
-	//	unordered_map<string, int >::iterator iter_out = output.begin();
-	//	unordered_map<string, vector<int>>::iterator iter_in;
-	//	for (; iter_out != output.end(); iter_out++)
-	//	{
-	//		//如果在输入表中找到了某个输出信号，则添加输出逻辑门指向输入逻辑门的边
-	//		iter_in = input.find(iter_out->first);
-	//		if (iter_in != input.end())
-	//		{
-	//			addEdge(iter_out->second, iter_in->second);
-	//		}
-
-	//	}
-
-	//}
-
+		for (auto& it : Gates)
+		{
+			if (visited.find((it.second).name) == visited.end())
+			{
+				DFSUtil((it.second).name, visited, recStack, path);
+			}
+		}
+	}
 };
 
 #endif // !CIRCUIT_H
--- Logic_Gate.h
+++ Logic_Gate.h
@@ -2,20 +2,19 @@
 #define LOGIC_GATE_H
 #include<string>
 #include<vector>
-using namespace std;
 
 class LogicGate {
 public:
-	string name;//逻辑门名称
+	std::string name;//逻辑门名称
 
-	vector<string> port;//输入输出信号
+	std::vector<std::string> port;//输入输出信号
 
 	int type;//逻辑门类型，从1开始，与、或、非、与非
 	bool isReverse;//是否含有反相器
 
 
 
-	LogicGate(string in_name, int in_type, string port0, string port1, string port2 = "")
+	LogicGate(std::string in_name, int in_type, std::string port0, std::string port1, std::string port2 = "")
 	{
 		name = in_name;
 		type = in_type;
